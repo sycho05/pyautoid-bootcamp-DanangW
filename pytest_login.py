2a23eaf1480043203ca0e8772d2d8701ea54144c
sycho05 <meowningworks@gmail.com>
2021-10-07 22:53:45 +0700

    final project

---
+++ pytest_login.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import pytest
+import time
+
+@pytest.fixture
+def setup():
+    driver = webdriver.Chrome()
+    driver.get('https://zateens.com/my-account-2/')
+    driver.implicitly_wait(10)
+    driver.maximize_window()
+    yield driver
+    driver.close()
+# send_keys('ximefo@musiccode.me')
+def test_login_success(setup): #testcase 001
+    setup.find_element_by_id('username').send_keys('jajang@mail.com')
+    setup.find_element_by_id('password').send_keys('mail123123_')
+    setup.find_element_by_name('login').click()
+    Badge = setup.find_elements_by_xpath('/html/body/div[1]/div[2]/div/div/main/article/div/div/div/p[1]/strong[1]')[0].text
+    assert Badge == 'jajang'
+    
+Accounts = [('jajang@smail.com','mail123123'),
+            ('jajang@mail.com','mail123123_'),
+            ('jajang@mail.com','mail123123_')]
+
+@pytest.mark.parametrize('a,b', Accounts)
+def test_login_unsuccess(setup,a,b): #testcase 002,003,004
+    setup.find_element_by_id('username').send_keys(a)
+    setup.find_element_by_id('password').send_keys(b)
+    setup.find_element_by_name('login').click()
+    Alert = setup.find_element_by_xpath('/html/body/div[1]/div[2]/div/div/main/article/div/div/nav/ul/li[6]/a').is_displayed()
+    assert Alert == True
--- sesi2.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-import time
-
-driver = webdriver.Chrome(executable_path='C:\chrome_driver\chromedriver.exe')
-driver.get('https://demoqa.com/links')
-driver.maximize_window()
-
-driver.find_element_by_link_text('Home').click()
